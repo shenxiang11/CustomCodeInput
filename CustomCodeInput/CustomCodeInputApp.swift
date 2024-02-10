8e73858b9d59f8a6b4862fd130d8c12405198b3d
Shen <863461783@qq.com>
2024-02-10 08:54:45 +0800

    Initial Commit

---
+++ CustomCodeInput/CustomCodeInputApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCodeInputApp.swift
+//  CustomCodeInput
+//
+//  Created by 香饽饽zizizi on 2024/2/10.
+//
+
+import SwiftUI
+
+@main
+struct CustomCodeInputApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
